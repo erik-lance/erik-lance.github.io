212e9ff67a21187a2b5897084f46099f0b24884e
erik-lance <eriklance@gmail.com>
2022-12-24 13:52:53 +0800

    Moved static files

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,9 +6,9 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 			alias: {
 				'@components': path.resolve(__dirname, 'src/components'),
 				'@styles': path.resolve(__dirname, 'src/styles'),
-				'@images': path.resolve(__dirname, 'src/images'),
-				'@fonts': path.resolve(__dirname, 'src/fonts'),
 				'@pages': path.resolve(__dirname, 'src/pages'),
+				'@images': path.resolve(__dirname, 'static/images'),
+				'@fonts': path.resolve(__dirname, 'static/fonts'),
 			}
 		}
 	});

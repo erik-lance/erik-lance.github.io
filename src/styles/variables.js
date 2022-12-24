81b1d1bb4be29e8f30f35c924a30c6333500e788
erik-lance <eriklance@gmail.com>
2022-12-24 13:52:38 +0800

    Added initial variables

---
+++ src/styles/variables.js
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+const variables = css`
+	:root {
+		// Colors
+		--black: #000000;
+		--gray: #404040;
+		--light-gray: #d9d9d9;
+		--dark-gray: #535353;
+		--green: #4ecb71;
+	}
+
+	* {
+		font-family: 'InterRegular';
+	}
+`;
+
+export default variables;

6019b550648aecd292a10d2c3a542d3012fb5fa9
erik-lance <eriklance@gmail.com>
2022-12-24 14:26:25 +0800

    (Prettier) and fixed path to images

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,23 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Erik Lance Tiongquico`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+	siteMetadata: {
+		title: `Erik Lance Tiongquico`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './static/img/',
+			},
+			__key: 'images',
+		},
+	],
 };

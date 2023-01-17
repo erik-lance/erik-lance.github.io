1897a9a74e88334a82a697b4bec36be6be40de6a
erik-lance <eriklance@gmail.com>
2023-01-17 19:31:35 +0800

    Adjusted details of hero statement

--- src/components/section/hero.js
+++ src/components/section/hero.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const StyledHeroSection = styled.section`
 	height: 600px;
 	width: 100%;
-	border: 4px solid var(--black);
+	/*border: 4px solid var(--black);*/
     display: flex;
     align-items: center;    
 
@@ -32,6 +32,11 @@ const StyledHeroSection = styled.section`
         font-size: 48px;
         color: var(--dark-gray);
     }
+
+    .hero-text:nth-child(4) {
+        font-size: 15px;
+        color: var(--dark-gray);
+    }
 `;
 
 const Hero = () => {
@@ -39,8 +44,15 @@ const Hero = () => {
 		<StyledHeroSection>
             <div id='hero-card'>
                 <p className='hero-text'>Hello! I am</p>
-                <p className='hero-text'>Erik Lance Tiongquico</p>
-                <p className='hero-text'>I'm a Lorem Ipsum but also dolor sit amet</p>
+                <p className='hero-text'>Erik Lance Tiongquico.</p>
+                <p className='hero-text'>I develop video games.</p>
+                <p className='hero-text'>
+                    I'm a student at De La Salle University and I
+                    join game jams when I can at &nbsp; 
+                    <a href="https://maplepoki.itch.io/" target="_blank">
+                    maplepoki.itch.io.
+                    </a>
+                </p>
             </div>
 		</StyledHeroSection>
 	);

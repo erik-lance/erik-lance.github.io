96dbd213e1bf12c1283741cb433855b5eb0877b2
erik-lance <eriklance@gmail.com>
2023-01-17 18:14:54 +0800

    Added initial about page

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 // Resolves to: src\components
 
 export { default as Hero } from './section/hero';
+export { default as About } from './section/about';
 export { default as Experience } from './section/experience';
 export { default as Contact } from './section/contact';
 export { default as Layout } from './layout';
---
+++ src/components/section/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledAboutSection = styled.section`
+`;
+
+const About = () => {
+    return(
+        <StyledAboutSection>
+            <h1>About</h1>
+        </StyledAboutSection>
+    )
+};
+
+export default About;

d89f062d855a9b13122f388842e6edc45f52a81e
erik-lance <eriklance@gmail.com>
2023-03-09 23:42:44 +0800

    Added text and fixed image bug

--- src/components/section/about.js
+++ src/components/section/about.js
@@ -14,13 +14,19 @@ const About = () => {
         <StyledAboutSection>
             <h1>About</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Howdy! I'm Erik Lance Tiongquico, a student at De La Salle University and a
+                Microsoft Learn Student Ambassador. I have always been programming since I was 10 years old
+                and love exploring new technologies. I'm currently learning React and Gatsby (this site was a product of it!).
+
             </p>
 
             <StyledImg>
-                <StaticImage>
-                    src="../../img/erik.jpg"
-                </StaticImage>
+                <StaticImage
+                    src="../../images/erik.jpg"
+                    alt="Erik Lance Tiongquico"
+                    width={300}
+                    height={300}
+                />
             </StyledImg>
         </StyledAboutSection>
     )
